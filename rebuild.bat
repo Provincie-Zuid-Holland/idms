dce542efaa7e78dcd12bdbd5a06a5d903f1aa42f
Daniel Overdevest <d.overdevest@pzh.nl>
2020-11-26 17:24:39 +0100

    Search API support

--- rebuild.bat
+++ rebuild.bat
@@ -1,5 +1,5 @@
 @echo on
-pip uninstall -y dist\opentext-0.1.5-py3-none-any.whl
+pip uninstall -y dist\opentext-0.1.6-py3-none-any.whl
 del /Q dist\
 python setup.py bdist_wheel
-pip install dist\opentext-0.1.5-py3-none-any.whl[dev]
+pip install dist\opentext-0.1.6-py3-none-any.whl[dev]
